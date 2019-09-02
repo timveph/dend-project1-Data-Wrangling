428c570473d3d76f1aeb0b8d69a9d6e59bca0930
Phillip <philliplearns@gmail.com>
2019-09-02 18:23:25 +0100

    saving work at end of day

---
+++ wrangle_act.py
@@ -0,0 +1,14 @@
+# imports
+import pandas as pd
+
+# variables
+
+
+# Read data (3 sources: twitter file, image predictions, twitter api extra data)
+
+# Read data - twitter file (csv)
+df_twitter_file = pd.read_csv("twitter-archive-enhanced.csv")
+print(df_twitter_file)
+
+
+
